8db4de5d9f918efc0272b71df7ca13da1e5f12eb
Yago-Pazos <ypazosl24@fpcoruna.afundacion.org>
2026-01-16 18:08:45 +0100

    Add initial models, migrations and admin configuration

--- DashBoard_Finanzas/finanzas/admin.py
+++ DashBoard_Finanzas/finanzas/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Categoria, Gasto, PresupuestoCategoria, ConfiguracionUsuario
+
 # Register your models here.
+
+admin.site.register(ConfiguracionUsuario)
+admin.site.register(Categoria)
+admin.site.register(Gasto)
+admin.site.register(PresupuestoCategoria)
--- DashBoard_Finanzas/finanzas/models.py
+++ DashBoard_Finanzas/finanzas/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+class ConfiguracionUsuario(models.Model):
+    ingresos_mensuales = models.DecimalField(decimal_places=2, max_digits=10)
+
+    presupuesto_total = models.DecimalField(decimal_places=2, max_digits=10)
+
+    fecha_inicio = models.DateTimeField()
+
+    def __str__(self):
+        return f"Configuración desde {self.fecha_inicio}"
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    color = models.CharField(max_length=20)
+
+    icono = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nombre
+
+class Gasto(models.Model):
+    importe = models.DecimalField(decimal_places=2, max_digits=10)
+
+    fecha = models.DateTimeField()
+
+    descripcion = models.CharField(max_length=100, blank= True)
+
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name="gastos")
+
+    def __str__(self):
+        return f"{self.categoria.nombre} - {self.importe}€"
+
+class PresupuestoCategoria(models.Model):
+    usuario = models.ForeignKey(
+        ConfiguracionUsuario,on_delete=models.CASCADE, related_name="presupuestos")
+
+    categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE,related_name="presupuestos")
+
+    porcentaje = models.PositiveIntegerField()
+
+    limite = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return f"{self.categoria.nombre} - {self.porcentaje}%"
+
+
+
